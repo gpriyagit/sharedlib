e34c4ac9ce6678727541aedb1ace6b4eca6cea5a
Amulyapriya <patoorupriya@gmail.com>
2025-04-04 17:59:36 +0200

    Update and rename deployapp.groovy to welcome.groovy

--- vars/deployapp.groovy
+++
@@ -1,6 +0,0 @@
-// vars/deployApp.groovy
-def call(Map config) {
-    echo "Deploying application to ${config.environment}"
-    // Logic for deploying the app based on the environment and config
-    sh "deploy --app ${config.appName} --env ${config.environment}"
-}
---
+++ vars/welcome.groovy
@@ -0,0 +1,5 @@
+// vars/welcome.groovy
+
+def call(String name = 'priya') {
+    echo "Hello, ${name}! This is a Jenkins Shared Library function."
+}
